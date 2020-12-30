d52148af982adc58e6f0daf793424a4022026c79
hguruman <hguruman@gmail.com>
2020-12-31 01:47:31 +0400

    delete unwanted files

--- app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ApplicationRecord
-  validates :postcontent, presence: true, length: { maximum: 500,
-                                                    too_long: '500 characters in post is the maximum allowed.' }
-  belongs_to :user, foreign_key: :user_id
-  has_many :replies, dependent: :destroy
-  has_many :likes, dependent: :destroy
-end
