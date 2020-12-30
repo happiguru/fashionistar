850185718e10484675c46f50b50c5e88bb580e9f
hguruman <hguruman@gmail.com>
2020-12-31 02:36:00 +0400

    Add validation for image and upload for image on the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :image, presence: true
+
+    has_one_attached :image, style: { :medium => "640x" }
+    validates_attached_content_type :image, :content_type => /\Aimage\/.*\Z/    
 end
